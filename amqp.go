22b98aa6bf89ce9a52872303c29667ae776172aa
Jaden Grossman <jadengrossman3@gmail.com>
2021-11-12 15:38:56 -0700

    Add decoding

--- amqp.go
+++ amqp.go
@@ -1,12 +1,10 @@
 package amqp
 
 import (
-	"bytes"
 	"fmt"
 	"time"
 
 	"github.com/Shimmur/proto_schemas_go/rpc"
-	"github.com/Shimmur/proto_schemas_go/types"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
@@ -64,7 +62,14 @@ type ListenOptions struct {
 	Args      amqpDriver.Table
 }
 
-func (amqp *Amqp) Encode(procedure string, destination string, source string) []byte {
+func (amqp *Amqp) Encode(procedure string, destination string, source string, params map[string]interface{}) []byte {
+	// m is a map[string]interface.
+	// loop over keys and values in the map.
+	for k, v := range params {
+		fmt.Println(k, "value is", v)
+	}
+	args := Map_to_struct(params)
+	fmt.Printf("build: %v \n", args)
 	event := &rpc.RPC{
 		Type:        rpc.RPC_WITH_REPLY,
 		Procedure:   procedure,
@@ -72,16 +77,17 @@ func (amqp *Amqp) Encode(procedure string, destination string, source string) []
 		Source:      source,
 		Id:          uuid.NewString(),
 		Timestamp:   time.Now().UnixMicro(),
-		Args: &types.Struct{
-			Fields: map[string]*types.Value{
-				"thread_id": {
-					Kind: &types.Value_StringValue{StringValue: "6025321b-7f3a-4f9c-99f9-9581c6e6f734"},
-				},
-				"sms_campaign_id": {
-					Kind: &types.Value_StringValue{StringValue: "51628409-300f-4beb-b18c-1f1baabbfa8a"},
-				},
-			},
-		},
+		Args:        args,
+		// Args: &types.Struct{
+		// 	Fields: map[string]*types.Value{
+		// 		"thread_id": {
+		// 			Kind: &types.Value_StringValue{StringValue: "6025321b-7f3a-4f9c-99f9-9581c6e6f734"},
+		// 		},
+		// 		"sms_campaign_id": {
+		// 			Kind: &types.Value_StringValue{StringValue: "51628409-300f-4beb-b18c-1f1baabbfa8a"},
+		// 		},
+		// 	},
+		// },
 	}
 
 	fmt.Printf("%+v\n", event)
@@ -94,7 +100,7 @@ func (amqp *Amqp) Encode(procedure string, destination string, source string) []
 	return msgbytes
 }
 
-func (amqp *Amqp) Decode(encodedMsg []byte) *types.Struct {
+func (amqp *Amqp) Decode(encodedMsg []byte) map[string]interface{} {
 	// var msg *rpc.RPCResponse // this is what you want
 	msg := &rpc.RPCResponse{} // all events on the bus are wrapped in this
 
@@ -104,16 +110,22 @@ func (amqp *Amqp) Decode(encodedMsg []byte) *types.Struct {
 		return nil
 	}
 
-	var buf bytes.Buffer
+	// var buf bytes.Buffer
 
-	err := pbJSONMarshaler.Marshal(&buf, msg)
+	// err := pbJSONMarshaler.Marshal(&buf, msg)
 
-	if err != nil {
-		fmt.Printf("failed to decode json: %v. error: %v", encodedMsg, err)
-		return nil
-	}
+	// if err != nil {
+	// 	fmt.Printf("failed to decode json: %v. error: %v", encodedMsg, err)
+	// 	return nil
+	// }
+	// m := make(map[string]interface{})
 
-	return msg.GetResponse()
+	// m["something"] = 1
+	// m["another"] = "hello there"
+	m := Struct_to_map(*msg.GetResponse())
+	fmt.Printf("hi: %v", m)
+	return m
+	// return msg.GetResponse()
 	// return buf.Bytes()
 }
 
---
+++ helper.go
@@ -0,0 +1,79 @@
+package amqp
+
+import (
+	"fmt"
+
+	"github.com/Shimmur/proto_schemas_go/types"
+)
+
+func Map_to_struct(m map[string]interface{}) *types.Struct {
+	var t = &types.Struct{}
+	var fields = make(map[string]*types.Value)
+
+	for k, v := range m {
+		typed := get_type(v)
+		fields[k] = typed
+	}
+	t.Fields = fields
+
+	return t
+}
+
+func Struct_to_map(s types.Struct) map[string]interface{} {
+	m := make(map[string]interface{})
+	for k, v := range s.Fields {
+		m[k] = get_real(v.Kind)
+	}
+	return m
+}
+
+//TODO: figure out the struct/list types
+func get_type(i interface{}) *types.Value {
+	switch i.(type) {
+	case bool:
+		return &types.Value{Kind: &types.Value_BoolValue{BoolValue: i.(bool)}}
+	case types.NullValue:
+		return &types.Value{Kind: &types.Value_NullValue{NullValue: i.(types.NullValue)}}
+	case int64:
+		return &types.Value{Kind: &types.Value_IntValue{IntValue: i.(int64)}}
+	case float64:
+		return &types.Value{Kind: &types.Value_DoubleValue{DoubleValue: i.(float64)}}
+	case string:
+		return &types.Value{Kind: &types.Value_StringValue{StringValue: i.(string)}}
+	default:
+		fmt.Printf("Couldnt get type for (%v), type:", i)
+		return nil
+	}
+}
+
+func get_real(i interface{}) interface{} {
+	switch i.(type) {
+	case *types.Value_StringValue:
+		return i.(*types.Value_StringValue).StringValue
+	case *types.Value_IntValue:
+		return i.(*types.Value_IntValue).IntValue
+	case *types.Value_DoubleValue:
+		return i.(*types.Value_DoubleValue).DoubleValue
+	case *types.Value_BoolValue:
+		return i.(*types.Value_BoolValue).BoolValue
+	case *types.Value_NullValue:
+		return i.(*types.Value_NullValue).NullValue
+	case *types.Value_ListValue:
+		var real []interface{}
+
+		for _, v := range i.(*types.Value_ListValue).ListValue.Values {
+			real = append(real, get_real(v.Kind))
+		}
+		return real
+	case *types.Value_StructValue:
+		var real = make(map[string]interface{})
+
+		for k, v := range i.(*types.Value_StructValue).StructValue.Fields {
+			real[k] = get_real(v.Kind)
+		}
+		return real
+	default:
+		fmt.Printf("Couldnt get real for (%v), type: %T\n", i, i)
+		return nil
+	}
+}
